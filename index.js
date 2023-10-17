16ac0a6a40bc1b3d65c8845bffaa704eecb59417
John Pieri <jvpieri@gmail.com>
2023-10-17 17:24:41 -0400

    Added fetch to prototype the API.

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const apiKey = "4ce09e3";
+const searchBar = document.getElementById('search-bar')
+const searchButton = document.getElementById('search-button')
+const form = document.querySelector('form')
 
-function fetchAPI() {
-  fetch(`http://www.omdbapi.com/?apikey=${apiKey}&t=Ants`, {method: "GET"})
-    .then((response) => response.json())
-    .then((data) => console.log(data));
+
+async function fetchAPI() {
+  let response = await fetch(`http://www.omdbapi.com/?apikey=${apiKey}&t=${searchBar.value}`, {method: "GET"})
+  let data = await response.json()
+  console.log(data)
 }
 
-fetchAPI()
-
+searchButton.addEventListener('click', (e) => {
+  e.preventDefault()
+  fetchAPI()
+})
