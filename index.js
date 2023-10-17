14b6101ca660f5b8ab0e92c15993d1c89a33bc88
John Pieri <jvpieri@gmail.com>
2023-10-17 14:53:32 -0400

    Commit

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-const apiKey = "bc32164e";
+const apiKey = "4ce09e3";
 
 function fetchAPI() {
-  fetch("http://www.omdbapi.com/?apikey=[bc32164e]&", {method: "GET"})
+  fetch(`http://www.omdbapi.com/?apikey=${apiKey}&t=Ants`, {method: "GET"})
     .then((response) => response.json())
     .then((data) => console.log(data));
 }
 
-fetchAPI();
+fetchAPI()
+
