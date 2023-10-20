667ce4572ebc0345f4714d943979f093d258188c
John Pieri <jvpieri@gmail.com>
2023-10-20 00:00:01 -0400

    Added switch for page placeholders

--- index.js
+++ index.js
@@ -1,16 +1,66 @@
 const apiKey = "4ce09e3";
 const searchBar = document.getElementById('search-bar')
 const searchButton = document.getElementById('search-button')
-const form = document.querySelector('form')
+const resultsContainerEl = document.querySelector('.results-container')
+let imbdIDArray = []
 
+document.addEventListener('DOMContentLoaded', () => {
+ renderPage()
+})
 
-async function fetchAPI() {
-  let response = await fetch(`http://www.omdbapi.com/?apikey=${apiKey}&t=${searchBar.value}`, {method: "GET"})
-  let data = await response.json()
-  console.log(data)
+document.addEventListener('click', (e) => {
+  if (e.target.matches('.submit-btn')) {
+    e.preventDefault()
+    let inputValue = searchBar.value
+    fetchSearchValue(inputValue)
+  }
+})
+
+async function fetchSearchValue(searchValue) {
+  let response = await fetch(`http://www.omdbapi.com/?apikey=${apiKey}&s=${searchValue}`, {method: "GET"})
+  
+  if (!response.ok) {
+    throw new Error ("Network response not ok")
+  } else {
+    let data = await response.json()
+    if (data.Response === "False") {
+      throw Error ("Movie not found!")
+    } else {
+      let searchResults = data.Search 
+      console.log(searchResults)
+      let
+    }
+  }
+
 }
 
-searchButton.addEventListener('click', (e) => {
-  e.preventDefault()
-  fetchAPI()
-})
+function renderSearchPagePlaceholder() {
+  resultsContainerEl.innerHTML = `
+  <div class="empty-search-wrapper">
+    <img src="./icons/Icon.svg" alt="a film icon" class="empty-search-icon">
+    <p>Start exploring</p>
+  </div>
+  `
+}
+
+function renderWatchlistPagePlaceholder() {
+  resultsContainerEl.innerHTML = `
+  <div class="empty-watchlist-wrapper">
+    <p>Your watchlist is looking a little empty</p>
+    <p><a href="index.html"><i class="fa-solid fa-circle-plus fa-lg"></i></a> Let's add some movies!</p>
+  </div>
+  `
+}
+
+function renderPage() {
+  let currentPage = document.body.id 
+  switch (currentPage) {
+    case "search-page":
+    renderSearchPagePlaceholder()
+    break
+    case "watchlist-page": 
+    renderWatchlistPagePlaceholder()
+    break
+  }
+}
+
